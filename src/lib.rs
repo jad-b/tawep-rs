b7745ea6d74b325a2e6e2198828e357cb6feb128
Jeremy Dobbins-Bucklad <jeremy.dobbins.bucklad@oracle.com>
2023-11-06 09:31:19 -0500

    Introduce 'rand' crate for rng

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,35 @@
 pub mod chapter2 {
+    use rand::Rng;
 
     pub fn substring_sort() {
-        // NEXT|
-        //   Define logic using type signatures and stubs
-
         // Char array of length l
         let l = 1 << 18;
         // Vector of char* of length n
-        let n = 1 << 14;
+        let _n = 1 << 14;
 
         // 1) Generate vector of string pointers
-        let vs = generate_string(l);
+        let _vs = generate_string(l);
         // 2) Execute comparison
         // 3) Output timing
 
     }
 
     fn generate_string(string_size: usize) -> String {
-        // rng = RNG()
-        /* for(
-         *   Initial condition:
-         *   - char* p = get the head value from the char array
-         *   - end = set to (init. mem pos. + 2^18)
-         *   Terminal condition:
-         *   - p reaches end
-         *   Update condition:
-         *   - p += num. of randgen'd bytes
-         * ) {
-         *   rand_data = rng.rando()
-         *   Overwrite array[p,p+sizeof(rand_data)] with rand_data
-         * }
-         */
         let s: Vec<u8> = Vec::with_capacity(string_size);
+        // NEXT|
+        // Fill vec with 'a' (utf8)
+        // Swap _some_ of the characters with random bytes
         return String::from_utf8(s).unwrap();
     }
 
-}
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-#[cfg(test)]
-mod tests {
-    // use super::*;
-
-    pub fn add(left: usize, right: usize) -> usize {
-        left + right
-    }
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        #[test]
+        fn string_generation_works() {
+            let result = generate_string(4);
+            assert_eq!(4, result.len(), "Generated a String of size {}", result.len());
+        }
     }
 }
