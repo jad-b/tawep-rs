fc0e84df3c4d0300ebe1c11e5ce5d70c00ebe1c7
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2023-11-09 06:41:22 -0500

    Randomly generated work

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,40 @@
+#![feature(ascii_char)]
+#![feature(ascii_char_variants)]
+
 pub mod chapter2 {
-    use rand::Rng;
+    use rand::{thread_rng, Rng};
+    use rand::distributions::{Distribution, Uniform};
+    use std::ascii::Char::{SmallA, SmallZ};
+
+    // Char array of length l
+    const L : usize = 1 << 18;
 
     pub fn substring_sort() {
-        // Char array of length l
-        let l = 1 << 18;
         // Vector of char* of length n
         let _n = 1 << 14;
 
         // 1) Generate vector of string pointers
-        let _vs = generate_string(l);
+        let _vs = generate_string();
         // 2) Execute comparison
         // https://doc.rust-lang.org/std/primitive.slice.html#method.sort
         // 3) Output timing
 
     }
 
-    fn generate_string(string_size: usize) -> String {
-        let s: Vec<u8> = Vec::with_capacity(string_size);
-        // NEXT|
-        // Fill vec with 'a' (utf8)
-        // Swap _some_ of the characters with random bytes
-        return String::from_utf8(s).unwrap();
+    fn generate_string() -> [u8; L] {
+        // Start with an array filled with 'a'
+        let s = [SmallA.to_u8(); L];
+        // Create a random lowercase ASCII generator
+        let ascii_rng = Uniform::from(SmallA.to_u8()..(SmallZ.to_u8()+1));
+        let mut rng = thread_rng();
+        // NEXT| Swap _some_ of the characters with random bytes
+        for i in 0..L>>10 {
+           let c = ascii_rng.sample(&mut rng);
+           let idx : u8 = rng.gen() % (L - 1);
+           s[idx] = c;
+        }
+
+        return s;
     }
 
     #[cfg(test)]
